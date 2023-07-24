7435eaa290894109fd1f44cdf1d4fa2ec9d40101
batuhanozkandev <batuhan.ozkan441@gmail.com>
2023-07-24 22:43:40 +0300

    add - beginning of file structure, start of boarding screen, font family

---
+++ lib/modules/boarding/presenter/boarding_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BoardingScreen extends StatelessWidget {
+  const BoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hi boarding screen'),
+      ),
+    );
+  }
+}
