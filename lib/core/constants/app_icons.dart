faa76cce8ece6fde1a91b3b7d4fd75ec08f69656
batuhanozkandev <batuhan.ozkan441@gmail.com>
2023-07-25 13:29:59 +0300

    add - static app icon and image paths

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,1 +1,34 @@
-
+class AppIcons {
+  static const mainPath = "assets/icons/";
+  static const String addCircle = '$mainPath/ic_add_circle.svg';
+  static const String arrowLeft = '$mainPath/ic_arrow_left.svg';
+  static const String arrowRight = '$mainPath/ic_arrow_right.svg';
+  static const String backArrow = '$mainPath/ic_back_arrow.svg';
+  static const String camera = '$mainPath/ic_camera.svg';
+  static const String cancel = '$mainPath/ic_cancel.svg';
+  static const String category = '$mainPath/ic_category.svg';
+  static const String categoryFilled = '$mainPath/ic_category_filled.svg';
+  static const String chat = '$mainPath/ic_chat.svg';
+  static const String delete = '$mainPath/ic_delete.svg';
+  static const String edit = '$mainPath/ic_edit.svg';
+  static const String facebook = '$mainPath/ic_facebook.svg';
+  static const String facebookFilled = '$mainPath/ic_facebook_filled.svg';
+  static const String filter = '$mainPath/ic_filter.svg';
+  static const String gallery = '$mainPath/ic_gallery.svg';
+  static const String global = '$mainPath/ic_global.svg';
+  static const String heart = '$mainPath/ic_heart.svg';
+  static const String hide = '$mainPath/ic_hide.svg';
+  static const String home = '$mainPath/ic_home.svg';
+  static const String homeFilled = '$mainPath/ic_home_filled.svg';
+  static const String instagram = '$mainPath/ic_instagram.svg';
+  static const String logOut = '$mainPath/ic_log_out.svg';
+  static const String notification = '$mainPath/ic_notification.svg';
+  static const String profile = '$mainPath/ic_profile.svg';
+  static const String profileFilled = '$mainPath/ic_profile_filled.svg';
+  static const String search = '$mainPath/ic_search.svg';
+  static const String send = '$mainPath/ic_send.svg';
+  static const String sendFilled = '$mainPath/ic_send_filled.svg';
+  static const String settings = '$mainPath/ic_settings.svg';
+  static const String show = '$mainPath/ic_show.svg';
+  static const String upload = '$mainPath/ic_upload.svg';
+}
--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,1 +1,20 @@
-
+class AppImages {
+  static const main = "assets/images/";
+  static const String background = '$main/background.svg';
+  static const String headerBackground = '$main/header_background.svg';
+  static const String dummyAvatar = '$main/dummy_avatar.svg';
+  static const String dummyAvatar2 = '$main/dummy_avatar_2.svg';
+  static const String dummyDesigner = '$main/dummy_designer_photo.svg';
+  static const String dummyIllustrator = '$main/dummy_illustrator_photo.svg';
+  static const String dummyMakingVideo = '$main/dummy_making_video_photo.svg';
+  static const String dummyPhoto = '$main/dummy_photo.svg';
+  static const String dummyPhoto2 = '$main/dummy_photo_2.svg';
+  static const String dummyPhoto3 = '$main/dummy_photo_3.svg';
+  static const String dummyPhotographer = '$main/dummy_photographer_photo.svg';
+  static const String dummyPhotographer2 =
+      '$main/dummy_photographer_photo_2.svg';
+  static const String dummyPostImage = '$main/dummy_post_image.svg';
+  static const String dummyPostImage2 = '$main/dummy_post_image2.svg';
+  static const String dummyUiDesign = '$main/dummy_ui_design_photo.svg';
+  static const String dummyVideoCreator = '$main/dummy_video_creator_photo.svg';
+}
