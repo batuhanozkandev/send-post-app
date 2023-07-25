0df5ac162bc31f13f6fe33dfbd1b29473be31479
batuhanozkandev <batuhan.ozkan441@gmail.com>
2023-07-25 12:51:28 +0300

    add - app icons and dummy photos

--- lib/modules/boarding/presenter/boarding_screen.dart
+++ lib/modules/boarding/presenter/boarding_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class BoardingScreen extends StatelessWidget {
   const BoardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Hi boarding screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hi boarding screen'),
+            SvgPicture.asset('assets/icons/ic_camera.svg'),
+          ],
+        ),
       ),
     );
   }
